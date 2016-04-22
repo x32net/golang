a6198d99e1585d86004c8dc9caba59068607c7b4
x32net <x32net@users.noreply.github.com>
2016-04-22 16:42:41 +0400

    Create stdin_pipe.go

---
+++ subprocess/stdin_pipe.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	subProcess := exec.Command("abc") // your subProcess
+	subProcessIn, err := subProcess.StdinPipe()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer subProcessIn.Close() // the doc says subProcess.Wait will close it, but I'm not sure, so I kept this line
+
+	subProcess.Stdout = os.Stdout
+	subProcess.Stderr = os.Stderr
+
+	fmt.Println("START")                      //for debug
+	if err = subProcess.Start(); err != nil { // Use start, not run
+		fmt.Println("An error occured: ", err)
+	}
+
+	io.WriteString(subProcessIn, "4\n")
+	subProcess.Wait()
+	fmt.Println("END") //for debug
+}
