8824bcdc017ea020cfcb12cbf23a529db6fe30c9
x32net <x32net@users.noreply.github.com>
2016-04-22 17:34:11 +0400

    Update web.go

--- subprocess/web.go
+++ subprocess/web.go
@@ -8,11 +8,11 @@ import (
 
 func main() {
 	subProcess := exec.Command("sub") // your subProcess
-	subProcessIn, err := subProcess.StdinPipe()
+	/*subProcessIn, err := subProcess.StdinPipe()
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer subProcessIn.Close() // the doc says subProcess.Wait will close it, but I'm not sure, so I kept this line
+	defer subProcessIn.Close()*/ // the doc says subProcess.Wait will close it, but I'm not sure, so I kept this line
 
 	var subProcessStdout bytes.Buffer
 	subProcess.Stdout = &subProcessStdout //os.Stdout
@@ -26,3 +26,15 @@ func main() {
 	fmt.Println("subProcessStderr", subProcessStderr.String())
 	fmt.Println("subProcessStdout", subProcessStdout.String())
 }
+
+/* http://stackoverflow.com/questions/8875038/redirect-stdout-pipe-of-child-process-in-golang
+Now I want to have the stdout of the child program in my terminal window where I started the parent program.
+No need to mess with pipes or goroutines, this one is easy.
+
+func main() {
+    cmd := exec.Command("ls", "-l") // Replace `ls` (and its arguments) with something more interesting
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    cmd.Run()
+}
+*/
